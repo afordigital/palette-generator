9237bad663a7ee250966f92b67dcad582ea483a2
fernando <fernandomoran323@gmail.com>
2024-11-21 23:36:13 -0800

    alias path configured

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,12 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src").toString(),
+      "@components": path.resolve(__dirname, "./src/components/").toString(),
+      "@assets": path.resolve(__dirname, "./src/assets/").toString(),
+      "@layouts": path.resolve(__dirname, "./src/layout/").toString(),
+      "@lib": path.resolve(__dirname, "./src/lib/").toString(),
+      "@utils": path.resolve(__dirname, "./src/utils/").toString(),
     },
   },
 });
