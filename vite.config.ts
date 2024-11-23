ce08d55a0e0eee427b0b4f63d7844d2cae2c8cc9
fernando <fernandomoran323@gmail.com>
2024-11-23 00:13:10 -0800

    Integrating the functionality of a PWA app

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
+import { manifestConfig } from './manifest';
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  base: "./",
+  plugins: [
+    react(),
+    VitePWA(manifestConfig)
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src").toString(),
